f70f4fb21267bb4803f528e3939c625e436b07f1
philspokas <phil@intellitect.com>
2022-02-17 08:20:36 -0800

    wip scratchy updates

--- github-tools/migrate-project.ps1
+++ github-tools/migrate-project.ps1
@@ -30,9 +30,28 @@ function Verify-TeamADGroups($targetProjectName) {
     return $false
 }
 
+function Get-ServiceConnection() {
+    #todo get the service connection for this org
+    # return it
+}
+
+function Migrate-Repos() {
+    #todo get repos
+    # for each $repo
+}
+
+function Migrate-Single-Repo($repoName) {
+    Exec { ./ado2gh lock-ado-repo --ado-org "intellitect-samples" --ado-team-project "AzureDevOpsDemo2018" --ado-repo "AzureDevOpsDemo2018.Utilities" }
+    Exec { ./ado2gh migrate-repo --ado-org $sourceOrg --ado-team-project $sourceProject --ado-repo $repoName --github-org $targetOrg --github-repo "$targetProjectName-$($repo.Name)" }
+    Exec { ./ado2gh configure-autolink --github-org "Intellitect-Samples" --github-repo "AzureDevOpsDemo2018-AzureDevOpsDemo2018.Utilities" --ado-org "intellitect-samples" --ado-team-project "AzureDevOpsDemo2018" }
+    AddTeamsToRepo($repoName)
+    Exec { ./ado2gh integrate-boards --ado-org "intellitect-samples" --ado-team-project "AzureDevOpsDemo2018" --github-org "Intellitect-Samples" --github-repo "AzureDevOpsDemo2018-AzureDevOpsDemo2018.Utilities" }
+    Exec { ./ado2gh rewire-pipeline --ado-org "intellitect-samples" --ado-team-project "AzureDevOpsDemo2018" --ado-pipeline "Utilities - CI" --github-org "Intellitect-Samples" --github-repo "AzureDevOpsDemo2018-AzureDevOpsDemo2018.Utilities" --service-connection-id "f14ae16a-0d41-48b7-934c-4a4e30be71e1" }
+}
+
 function Create-GHTeams() {
 
-#    Exec { ado2gh create-team --github-org $targetOrg --team-name "$targetProjectName" --idp-group "GL-OGH-$targetProjectName" }
+    Exec { ado2gh create-team --github-org $targetOrg --team-name "$targetProjectName" --idp-group "GL-OGH-$targetProjectName" }
     Exec { ado2gh create-team --github-org $targetOrg --team-name "$targetProjectName-Admins" --idp-group "GL-OGH-$targetProjectName-Admins" }
     Exec { ado2gh create-team --github-org $targetOrg --team-name "$targetProjectName-Readers" --idp-group "GL-OGH-$targetProjectName-Readers" }
     Exec { ado2gh create-team --github-org $targetOrg --team-name "$targetProjectName-Contributors" --idp-group "GL-OGH-$targetProjectName-Contributors" }
