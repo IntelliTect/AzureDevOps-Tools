f0628e0003e5b263d6ffaa22f321efe82cec54fd
Zack <zack.ward@intellitect.com>
2022-10-28 11:26:35 -0700

    updated default batch size

--- tools/generateAgentSummaries.ps1
+++ tools/generateAgentSummaries.ps1
@@ -3,7 +3,7 @@
     [string]$sourceOrg, 
     [string]$poolType, #poolTypes are "all" or "ad", "automated" or "a", "devlopment or d"
     [string]$OutFile, 
-    [int]$BatchSize = 1,
+    [int]$BatchSize = 10,
     [string]$LogLocation = $PSScriptRoot
 )
 . .\AzureDevOps-Helpers.ps1 -LogLocation $LogLocation
