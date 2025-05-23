249c0ac73d168e938fd7b19df10c55a5339733da
8bitbender <john@bytebender.io>
2025-05-23 16:40:40 -0700

    list endpoint roles and filter out Endpoint Administrators (default)

--- modules/Migrate-ADO-ServiceConnections.psm1
+++ modules/Migrate-ADO-ServiceConnections.psm1
@@ -20,7 +20,7 @@ function Start-ADOServiceConnectionsMigration {
         Write-Log -Message '---------------------------------------------'
         Write-Log -Message ' '
 
-        # $sourceProject = Get-ADOProjects -OrgName $SourceOrgName -ProjectName $SourceProjectName -Headers $SourceHeaders 
+        $sourceProject = Get-ADOProjects -OrgName $SourceOrgName -ProjectName $SourceProjectName -Headers $SourceHeaders 
         $targetProject = Get-ADOProjects -OrgName $TargetOrgName -ProjectName $TargetProjectName -Headers $TargetHeaders 
         
         $sourceEndpoints = Get-ServiceEndpoints -OrgName $SourceOrgName -ProjectName $SourceProjectName  -Headers $sourceHeaders
@@ -30,12 +30,12 @@ function Start-ADOServiceConnectionsMigration {
         
         foreach ($endpoint in $sourceEndpoints) {
         
-            if ($null -ne ($targetEndpoints | Where-Object {$_.description.ToUpper().Contains("#ORIGINSERVICEENDPOINTID:$($endpoint.id.ToUpper())")})) {
+            if ($null -ne ($targetEndpoints | Where-Object { $_.description.ToUpper().Contains("#ORIGINSERVICEENDPOINTID:$($endpoint.id.ToUpper())") })) {
                 Write-Log -Message "Service endpoint [$($endpoint.id)] already exists in target.. "
                 continue
             }
 
-            if ($null -ne ($targetEndpoints | Where-Object {($_.name -eq $endpoint.name) -and ($_.type -eq $endpoint.type)})) {
+            if ($null -ne ($targetEndpoints | Where-Object { ($_.name -eq $endpoint.name) -and ($_.type -eq $endpoint.type) })) {
                 Write-Log -Message "Service endpoint [$($endpoint.name)] [$($endpoint.id)] already exists in target.. "
                 continue
             }
@@ -43,27 +43,27 @@ function Start-ADOServiceConnectionsMigration {
             Write-Log -Message "Attempting to create [$($endpoint.name)] in target.. "
 
             $projectReference = @{
-                "id"        = $targetProject.id
-                "name"      = $TargetProjectName
+                "id"   = $targetProject.id
+                "name" = $TargetProjectName
             }
 
             $endpointProjectReference = @{
-                "name"              = $endpoint.name
-                "description"       = ""
-                "projectReference"  = $projectReference
+                "name"             = $endpoint.name
+                "description"      = ""
+                "projectReference" = $projectReference
             }
 
             $endpoint.serviceEndpointProjectReferences = @($endpointProjectReference)
         
-            if($endpoint.data.creationMode -eq "Automatic") {
-                if($null -ne $endpoint.data.azureSpnRoleAssignmentId){
+            if ($endpoint.data.creationMode -eq "Automatic") {
+                if ($null -ne $endpoint.data.azureSpnRoleAssignmentId) {
                     $endpoint.data.azureSpnRoleAssignmentId = $null
                 }
                 $endpoint.data.azureSpnPermissions = $null
                 $endpoint.data.spnObjectId = $null
                 $endpoint.data.appObjectId = $null
                 
-                if($NULL -ne $endpoint.authorization.parameters.authenticationType) {
+                if ($NULL -ne $endpoint.authorization.parameters.authenticationType) {
                     $endpoint.authorization.parameters.authenticationType = $NULL
                 }
             }
@@ -75,54 +75,61 @@ function Start-ADOServiceConnectionsMigration {
                 }
                 Write-Log "Service Connection $($endpoint.name) requires configuration post-migration with a valid GitHub access token."
                 $endpoint.authorization | Add-Member -NotePropertyName parameters -NotePropertyValue $parameters
-            } elseif ($endpoint.type -eq "azurerm") {
+            }
+            elseif ($endpoint.type -eq "azurerm") {
                 # Azurerm Service Connection types will need to be edited after migration to adhere to org/project naming conventions.
-                if($endpoint.authorization.scheme -eq "WorkloadIdentityFederation" -OR $endpoint.authorization.scheme -eq "ServicePrincipal") {
+                if ($endpoint.authorization.scheme -eq "WorkloadIdentityFederation" -OR $endpoint.authorization.scheme -eq "ServicePrincipal") {
                     $parameters = @{
-                        "tenantid" = $endpoint.authorization.parameters.tenantid
+                        "tenantid"           = $endpoint.authorization.parameters.tenantid
                         "serviceprincipalid" = $endpoint.authorization.parameters.serviceprincipalId
                     }
-                    if($null -eq $endpoint.authorization.parameters){
+                    if ($null -eq $endpoint.authorization.parameters) {
                         $endpoint.authorization | Add-Member -NotePropertyName parameters -NotePropertyValue $parameters
-                    } else {
+                    }
+                    else {
                         $endpoint.authorization.parameters = $parameters
                     }
                     $endpoint.data.CreationMode = "Manual"
                     $endpoint.authorization.scheme = "WorkloadIdentityFederation"
                 }
-                elseif($endpoint.authorization.scheme -eq "PublishProfile") {
+                elseif ($endpoint.authorization.scheme -eq "PublishProfile") {
                     $parameters = @{
-                        "tenantid" = $tenantId
+                        "tenantid"   = $tenantId
                         "resourceId" = $endpoint.authorization.resourceId
                     }
                     Write-Log "Endpoint: $($endpoint.name)"
                     Write-Log "Auth params: $($parameters.tenantId)"
-                    if($null -eq $endpoint.authorization.parameters){
+                    if ($null -eq $endpoint.authorization.parameters) {
                         $endpoint.authorization | Add-Member -NotePropertyName parameters -NotePropertyValue $parameters
-                    } else {
+                    }
+                    else {
                         $endpoint.authorization.parameters = $parameters
                     }
                 } 
 
-            } elseif ($endpoint.type -eq "externaltfs") {
+            }
+            elseif ($endpoint.type -eq "externaltfs") {
                 $parameters = @{
                     "apitoken" = "0123456789" 
                 }
                 Write-Log "Service Connection $($endpoint.name) requires configuration post-migration with a valid API token."
                 $endpoint.authorization | Add-Member -NotePropertyName parameters -NotePropertyValue $parameters
-            } elseif ($endpoint.type -eq "stormrunner") {
-                 $endpoint.authorization.parameters.username = "abcdefghij"
-                 $endpoint.authorization.parameters | Add-Member -NotePropertyName password -NotePropertyValue "0123456789" 
-                 Write-Log "Service Connection $($endpoint.name) requires configuration post-migration with a valid username and password."
+            }
+            elseif ($endpoint.type -eq "stormrunner") {
+                $endpoint.authorization.parameters.username = "abcdefghij"
+                $endpoint.authorization.parameters | Add-Member -NotePropertyName password -NotePropertyValue "0123456789" 
+                Write-Log "Service Connection $($endpoint.name) requires configuration post-migration with a valid username and password."
 
-            } elseif ($endpoint.type -eq "OctopusEndpoint") {
+            }
+            elseif ($endpoint.type -eq "OctopusEndpoint") {
                 $parameters = @{
                     "apitoken" = "0123456789" 
                 }
                 Write-Log "Service Connection $($endpoint.name) requires configuration post-migration with an API token."
 
                 $endpoint.authorization | Add-Member -NotePropertyName parameters -NotePropertyValue $parameters
-            } elseif ($endpoint.type -eq "sonarqube") {
+            }
+            elseif ($endpoint.type -eq "sonarqube") {
                 $parameters = @{
                     "username" = "abcdefghij"
                 }
@@ -136,6 +143,8 @@ function Start-ADOServiceConnectionsMigration {
             try {
                 New-ServiceEndpoint -OrgName $TargetOrgName -ProjectName $TargetProjectName -Headers $targetHeaders -ServiceEndpoint $endpoint
                 Write-Log -Message "Done!" -LogLevel SUCCESS
+
+                Start-ADOServiceConnectionRolesMigration -SourceProjectId $sourceProject.id -TargetProjectId $targetProject.id -EndpointId $endpoint.id
             }
             catch {
                 Write-Log -Message "FAILED!" -LogLevel ERROR
@@ -168,5 +177,36 @@ function New-ServiceEndpoint([string]$OrgName, [string]$ProjectName, $Headers, $
     $results = Invoke-RestMethod -ContentType "application/json" -Method Post -uri $url -Headers $Headers -Body $body 
     
     return $results
+}
 
+function Start-ADOServiceConnectionRolesMigration([string] $SourceProjectId, [string] $TargetProjectId, [string] $EndpointId) {
+    $sourceRoleAssignments = Get-RoleAssignments -OrgName $SourceOrgName -ProjectId $SourceProjectId -EndpointId $EndpointId -Headers $SourceHeaders
+
+    foreach ($roleAssignment in $sourceRoleAssignments) {
+        $roleName = $roleAssignment.identity.uniqueName
+
+        if($roleName -notlike "*Endpoint Administrators") {
+            
+            Write-Log -Message "Attempting to create role assignment [$($roleName)] in target.. "
+    
+            # try {
+            #     New-RoleAssignment -OrgName $TargetOrgName -ProjectId $ProjectId -EndpointId $EndpointId -Headers $TargetHeaders -RoleAssignment $roleAssignment
+            #     Write-Log -Message "Done!" -LogLevel SUCCESS
+            # }
+            # catch {
+            #     Write-Log -Message "FAILED!" -LogLevel ERROR
+            #     Write-Log -Message $_.Exception -LogLevel ERROR
+            #     Write-Log -Message $_ -LogLevel ERROR
+            #     Write-Log -Message " "
+            # }
+        }
+    }
+}
+
+function Get-RoleAssignments([string]$OrgName, [string] $ProjectId, [string] $EndpointId, $Headers) {
+    $url = "https://dev.azure.com/$OrgName/_apis/securityroles/scopes/distributedtask.serviceendpointrole/roleassignments/resources/{0}_{1}" -f $ProjectId, $EndpointId
+
+    $results = Invoke-RestMethod -ContentType "application/json" -Method Get -uri $url -Headers $Headers 
+    
+    return , $results.value
 }
